6f104b6e754538a9cb3f3938de70688c698a8150
Kipkerich <jkipkerich@kabarak.ac.ke>
2025-04-21 16:41:10 +0300

    styled the pages and modified the views

--- main/views.py
+++ main/views.py
@@ -2,32 +2,33 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def signup_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        email = request.POST['email']
-        first_name = request.POST['first_name']
-        last_name = request.POST['last_name']
-
-        user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
-        user.save()
-        return redirect('login')
-    return render(request, 'accounts/signup.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
             login(request, user)
             return redirect('index')
         else:
-            return render(request, 'login.html', {'error': 'Invalid username or password'})
-    return render(request, 'accounts/login.html')
+            return render(request, 'accounts/login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
